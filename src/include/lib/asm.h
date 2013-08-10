ceed55091f37dd7296832fbfa9a47d555b7f38d5
Martin Miralles-Cordal <axalon900@gmail.com>
2013-08-10 01:35:56 -0400

    more inline asm functions

--- src/include/lib/asm.h
+++ src/include/lib/asm.h
@@ -1,12 +1,22 @@
+#ifndef ASM_H
+#define ASM_H
+
 #include <decl.h>
+#include <stddef.h>
+#include <stdint.h>
 
 __BEGIN_DECLS
 
-#define outb(port, value) \
-do { \
-	unsigned short pt = (port); \
-	unsigned char val = (value); \
-    asm volatile( "outb %0, %1" : : "a"(val), "Nd"(pt) ); \
-} while(0)
+inline uint8_t inb(uint16_t port);
+inline uint8_t inw(uint16_t port);
+inline uint8_t inl(uint16_t port);
+
+inline void outb(unsigned short port, uint8_t val);
+inline void outw(unsigned short port, uint16_t val);
+inline void outl(unsigned short port, uint32_t val);
+
+inline void cpuid(int32_t code, uint32_t *a, uint32_t *d);
 
 __END_DECLS
+
+#endif
