e4f2a77d4b37900530618f20af4d5edd7ac5b93d
Martin Miralles-Cordal <axalon@heliarc.net>
2013-09-02 22:30:59 -0400

    Gave palloc()/pfree() default page count of 1.

--- src/kernel/include/mem/MMU.hpp
+++ src/kernel/include/mem/MMU.hpp
@@ -6,8 +6,8 @@ class MMU
 {
 public:
     MMU(uint32_t mmap_addr, uint32_t mmap_length);
-    void *palloc(size_t numberOfPages);
-    int pfree(void *startOfMemoryRange, size_t numberOfPages);
+    void *palloc(size_t numberOfPages = 1);
+    int pfree(void *startOfMemoryRange, size_t numberOfPages = 1);
     void install();
 private:
     PageFrameAllocator _pageFrameAllocator;
