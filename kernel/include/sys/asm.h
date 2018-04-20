6af5575444957088b0472e79acc4e0ab44457c4d
Martin Miralles-Cordal <axalon@rutgers.edu>
2018-04-20 01:38:13 +0000

    Fixed types for inb, inw, and inl

--- kernel/include/sys/asm.h
+++ kernel/include/sys/asm.h
@@ -38,7 +38,7 @@ static inline void cpuid(int32_t code, uint32_t *a, uint32_t *d)
 
 static inline uint8_t inb(uint16_t port)
 {
-    unsigned char ret;
+    uint8_t ret;
     asm volatile( "inb %1, %0"
                   : "=a"(ret) : "Nd"(port) );
     return ret;
@@ -46,7 +46,7 @@ static inline uint8_t inb(uint16_t port)
 
 static inline uint16_t inw(uint16_t port)
 {
-    unsigned char ret;
+    uint16_t ret;
     asm volatile( "inw %1, %0"
                   : "=a"(ret) : "Nd"(port) );
     return ret;
@@ -54,7 +54,7 @@ static inline uint16_t inw(uint16_t port)
 
 static inline uint32_t inl(uint16_t port)
 {
-    unsigned char ret;
+    uint32_t ret;
     asm volatile( "inl %1, %0"
                   : "=a"(ret) : "Nd"(port) );
     return ret;
