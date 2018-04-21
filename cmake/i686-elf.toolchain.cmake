48499cafaab6578f6ae21776a6cb4e38c2681388
Martin Miralles-Cordal <axalon@rutgers.edu>
2018-04-21 17:19:32 +0000

    Added an i686-elf toolchain file.

---
+++ cmake/i686-elf.toolchain.cmake
@@ -0,0 +1,17 @@
+# Toolchain for i586-elf cross compiler
+
+set (CMAKE_SYSTEM_NAME Generic)
+
+set (CMAKE_C_COMPILER i686-elf-gcc)
+set (CMAKE_CXX_COMPILER i686-elf-g++)
+set (CMAKE_ASM_ATT_COMPILER i686-elf-as)
+
+# GCC was built as follows:
+#
+# ../configure --target=i586-elf --prefix="<prefix>" --with-gmp=/usr/local --with-mpfr=/usr/local  --with-mpc=/usr/local
+#              --disable-nls --enable-languages=c,c++ --without-headers
+#
+# Thus, it will probably fail to compile the test apps since it doesn't include headers or libs or anything like that.
+# Therefore, force cmake to skip the test.
+set(CMAKE_C_COMPILER_WORKS 1)
+set(CMAKE_CXX_COMPILER_WORKS 1)
