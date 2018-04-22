07201275e61f7f7681ef7b475c2f1dba6ccc5be9
Martin Miralles-Cordal <axalon@rutgers.edu>
2018-04-22 02:29:24 +0000

    Added Array object that boxes heap arrays.
    Removed isEqualTo() because we can't dynamic_cast to make it work anyway.

--- libsys/include/Object.hpp
+++ libsys/include/Object.hpp
@@ -11,21 +11,39 @@ public:
     void retain() const { ++_referenceCount; }
     void release() const
     {
-        --_referenceCount;
-
-        if (_referenceCount == 0) {
+        if (--_referenceCount == 0) {
             delete (Object *) this;
         }
     }
-
-    virtual bool isEqualTo(Object *o) { return this == o; }
 
 private:
     mutable int _referenceCount;
 };
 
-/** Fucky version of shared_ptr, except it only works on objects of Object type. I'd SFINAE but lol no stdlib yet. */
-template <typename T> class ArcPtr {
+template <typename T> class Array : public Object
+{
+  public:
+    Array(size_t size) : _array(new T[size]) {}
+    ~Array() { delete[] _array; }
+
+    T *get() { return _array; }
+    const T *get() const { return _array; }
+    operator const T*() const { return get(); }
+    operator T*() { return get(); }
+
+    T & operator[](size_t idx) { return _array[idx]; }
+    T const & operator[](size_t idx) const { return _array[idx]; }
+
+  private:
+    T *_array;
+};
+
+/**
+ * Fucky version of shared_ptr, except it only works on objects of Object type.
+ * I'd SFINAE but lol no stdlib yet.
+ */
+template <typename T> class ArcPtr
+{
   public:
     ArcPtr(T *ptr = nullptr) : _ptr(ptr) { if (ptr) ptr->retain(); }
 
