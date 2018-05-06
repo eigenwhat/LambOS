ff03399d9fe0dc26976868a12e820a3e92527128
Martin Miralles-Cordal <axalon@rutgers.edu>
2018-05-06 16:11:46 -0400

    Implemented ArrayList iterator.

--- libsys/include/util/ArrayList.hpp
+++ libsys/include/util/ArrayList.hpp
@@ -3,10 +3,38 @@
 #include <util/DynamicArray.hpp>
 #include <util/List.hpp>
 
+/**
+ * An ArrayList is an ordered list where the elements are stored in a contiguous
+ * array, which is dynamically resized as needed.
+ *
+ * ArrayLists excel at random access due to the contents being stored in a
+ * contiguous block of memory, compared to LinkedLists, which need to traverse
+ * node by node to get to any one element. ArrayLists are also faster to iterate
+ * though for the same reason. When the needed space is available, insertion and
+ * removal of elements at the end of the ArrayList is very fast and requires no
+ * further heap allocations.
+ *
+ * ArrayLists suffer in insertion and removal of elements anywhere far from the
+ * end of the list, as the contents need to be shifted to maintain order and
+ * contiguousness. Additionally, once reaching the allocated capacity, the
+ * memory block needs to be grown if any new elements are to be inserted. This
+ * requires allocating a new, larger memory block and copying over the old
+ * contents, which is expensive. This is mitigated somewhat by the ArrayList's
+ * default behavior of allocating a new block double the size of the current
+ * block, allowing many insertions before the next resizing event. This comes at
+ * the expense of memory efficiency, as the list takes up more space than
+ * strictly necessary. This can be further mitigated if the creator of the list
+ * knows beforehand roughly how much space will be needed and can then pre-
+ * allocate an appropriate amount of space.
+ *
+ * @tparam T The type of object the ArrayList holds.
+ */
 template <typename T>
 class ArrayList : public List<T>
 {
   public:
+    using Iterator = typename Iterable<T>::Iterator;
+
     /** Constructs an ArrayList with the default pre-allocation. */
     ArrayList() : ArrayList(8) {}
 
@@ -185,6 +213,27 @@ class ArrayList : public List<T>
      */
     T& operator[](size_t idx) const override { return _data[idx]; }
 
+    /** An iterator pointing to the first element in the list. */
+    Iterator begin() const override
+    {
+        auto *llit = new ALIterator{0, this};
+        autorelease(llit);
+        return Iterable<T>::newIterator(llit);
+    }
+
+    /**
+     * An Iterator "past the end" of the list. When encountered, it means there
+     * is nothing else to read and iteration should stop.
+     *
+     * @return An iterator signifying the end of this ArrayList.
+     */
+    Iterator end() const override
+    {
+        auto *llit = new ALIterator{npos, this};
+        autorelease(llit);
+        return Iterable<T>::newIterator(llit);
+    }
+
     /**
      * The maximum capacity of the currently allocated block.
      * @return The size of the underlying memory block.
@@ -192,6 +241,7 @@ class ArrayList : public List<T>
     size_t capacity() const { return _data.capacity(); }
 
   private:
+    static constexpr size_t npos = -1;
     /**
      * Shifts the data back. Resizes if necessary so no data gets lost.
      * @param offset The amount to shift the data right by.
@@ -211,6 +261,80 @@ class ArrayList : public List<T>
             _data.shift(offset);
         }
     }
+
+    class ALIterator : public Iterable<T>::IteratorImpl
+    {
+        friend class ArrayList;
+        using Iterable<T>::IteratorImpl::publicInstance;
+        using Iterable<T>::IteratorImpl::implOf;
+      public:
+        /**
+         * (pre-increment) Advances the iterator one element forward.
+         * @return A reference to the next Iterator in the Collection.
+         */
+        Iterator& operator++() override
+        {
+            ++_index;
+            if (_index == _parent->size()) {
+                _index = npos;
+            }
+
+            return *publicInstance();
+        }
+
+        /**
+         * (post-increment) Advances the Iterator one element forward.
+         * @return An Iterator equal to this before the increment.
+         */
+        Iterator operator++(int) override
+        {
+            ALIterator *it = new ALIterator(*this);
+            autorelease(it);
+            this->operator++();
+            return Iterable<T>::newIterator(it);
+        }
+
+        /**
+         * Retrieves a reference to the pointed-to element.
+         * @return A reference to the element the Iterator is pointing at. If it
+         *         isn't pointing to anything (e.g. end of the Collection), the
+         *         result is undefined.
+         */
+        T& operator*() const override { return (*_parent)[_index]; }
+
+        /**
+         * Equality operator.
+         * @param rhs The iterator to compare to.
+         * @return `true` if they are equal, `false` otherwise.
+         */
+        bool operator==(Iterator const &rhs) const override
+        {
+            if (classId() != rhs.classId()) {
+                return false;
+            }
+
+            ALIterator *iterator = reinterpret_cast<ALIterator *>(implOf(rhs));
+            if (!iterator) {
+                return false;
+            }
+
+            if (iterator == this) {
+                return true;
+            }
+
+            return _index == iterator->_index && _parent == iterator->_parent;
+        }
+
+        size_t classId() const override { return 0xA88A1757; }
+
+      private:
+        ALIterator(size_t index, ArrayList const *parent)
+                : _index(index), _parent(parent)
+        {}
+
+        size_t _index;
+        ArrayList const *_parent;
+    };
 
     DynamicArray<T> _data;
     size_t _size = 0;
