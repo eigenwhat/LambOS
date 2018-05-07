039a537268e451319c4869052299bef37cfefc8e
Martin Miralles-Cordal <axalon@rutgers.edu>
2018-05-06 20:54:20 -0400

    Added Path class.

---
+++ libsys/include/fs/Path.hpp
@@ -0,0 +1,61 @@
+#pragma once
+
+#include <util/ArrayList.hpp>
+#include <util/String.hpp>
+
+/**
+ * A Path represents a possible path on a filesystem. No checks are made as to
+ * whether or not the path exists on a filesystem; it handles only the
+ * syntactical aspects of a path.
+ */
+class Path : public Object
+{
+  public:
+    /**
+     * Creates a Path object with the given String.
+     * @param str The String to process.
+     */
+    Path(String const &str) : Path(str.cstr()) {}
+
+    /**
+     * Creates a Path object with the given C string.
+     * @param str The C string to process.
+     */
+    Path(char const *str);
+
+    /**
+     * Returns the Path as a String.
+     * @return The stringified path.
+     */
+    String pathString() const;
+
+    /**
+     * Returns the last path component.
+     * @return A String representation of the last path component.
+     */
+    String const &lastPathComponent() const;
+
+    /**
+     * Returns the file extension of the last path component, if the path is to
+     * a file.
+     * @return The extension. (e.g. "zip" if lastPathComponent() == "file.zip")
+     */
+    String extension() const;
+
+    /**
+     * Returns whether or not the path is absolute (e.g. starts with "/")
+     * @return `true` if absolute, `false` otherwise.
+     */
+    bool isAbsolute() const { return _isAbsolute; }
+
+    /**
+     * Returns whether of not the path ends with a delimiter. (e.g. "/")
+     * @return `true` if the path points to a directory, `false` otherwise.
+     */
+    bool isDirectory() const { return _isDirectory; }
+
+  private:
+    ArrayList<String> _pathComponents;
+    bool _isAbsolute = false;
+    bool _isDirectory = false;
+};
---
+++ libsys/src/fs/Path.cpp
@@ -0,0 +1,76 @@
+#include <fs/Path.hpp>
+#include <util/Array.hpp>
+#include <util/StringTokenizer.hpp>
+
+namespace {
+
+static constexpr char const kDelims[] = "/\\";
+
+bool isAbsolutePath(char const *str)
+{
+    for (auto c : kDelims) {
+        if (str[0] == c) return true;
+    }
+
+    return false;
+}
+
+}
+
+Path::Path(char const *str)
+{
+    String s {str};
+    if (!s.size()) {
+        return;
+    }
+
+    _isAbsolute = isAbsolutePath(str);
+
+    StringTokenizer tokenizer {s, kDelims};
+    while (tokenizer.hasNextToken()) {
+        _pathComponents.enqueue(tokenizer.nextToken());
+    }
+}
+
+String Path::pathString() const
+{
+    String result;
+
+    if (_isAbsolute) {
+        result.append('/');
+    }
+
+    for (String &c : _pathComponents) {
+        result.append(c);
+        if (c != _pathComponents.peekBack()) {
+            result.append('/');
+        }
+    }
+
+    return result;
+}
+
+String const &Path::lastPathComponent() const
+{
+    if (_pathComponents.isEmpty()) {
+        return String::emptyString();
+    }
+
+    return _pathComponents.peekBack();
+}
+
+String Path::extension() const
+{
+    if (_isDirectory) { return String::emptyString(); }
+
+    auto &s = lastPathComponent();
+    if (!s.size()) { return String::emptyString(); }
+
+    StringTokenizer tokenizer {s, "."};
+    String token;
+    while (tokenizer.hasNextToken()) {
+        token = tokenizer.nextToken();
+    }
+
+    return token;
+}
