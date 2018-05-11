3f84f9ad2f79b79d00bd381bd54e5ddc6a798ff1
Martin Miralles-Cordal <axalon@rutgers.edu>
2018-05-10 21:45:54 -0400

    Added reserve() function.

--- libsys/include/util/ArrayList.hpp
+++ libsys/include/util/ArrayList.hpp
@@ -46,6 +46,19 @@ class ArrayList : public List<T>
     ArrayList(size_t reserve) : _data(reserve) {}
 
     /**
+     * Reserves enough room to hold the given amount.
+     * This function can only grow the ArrayList. If reserve <= capacity, no
+     * action is performed.
+     * @param reserve The number of elements to make room for.
+     */
+    void reserve(size_t reserve)
+    {
+        if (reserve > capacity()) {
+            _data.resize(reserve);
+        }
+    }
+
+    /**
      * Adds an element to the top of the ArrayList.
      * @note This is an O(n) operation! If you're doing this often, consider
      *       the ordering of your data, or use a different List implementation.
