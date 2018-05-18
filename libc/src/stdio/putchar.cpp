4b0e930c7ef3e45e1cc3bdc70a345e4bcdc71438
Martin Miralles-Cordal <axalon@rutgers.edu>
2018-05-17 21:28:22 -0400

    Added "KERNEL" define. Added libk target, which is libc + KERNEL def.

--- libc/src/stdio/putchar.cpp
+++ libc/src/stdio/putchar.cpp
@@ -1,13 +1,17 @@
-#include <Kernel.hpp>
 #include <stdio.h>
+
+#ifdef KERNEL
+#include <Kernel.hpp>
+#endif
 
 extern "C" {
 
 int putchar(int ic)
 {
+#ifdef KERNEL
     char c = (char) ic;
     kernel->out()->print(c);
-
+#endif
     return ic;
 }
 
