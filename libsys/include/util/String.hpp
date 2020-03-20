40620757fa32813413d6e78ce4bcba503fa75c92
Martin Miralles-Cordal <axalon@rutgers.edu>
2020-03-19 22:17:07 -0400

    Broke off base prefix creation to its own function.

--- libsys/include/util/String.hpp
+++ libsys/include/util/String.hpp
@@ -60,6 +60,41 @@ void append_string(String &s, Int num, unsigned int base)
 
 inline int clamp_base(unsigned int base) { return static_cast<int>(std::clamp(base, 2u, 32u)); }
 
+// returns past-the-end of added prefix
+inline char * base_prefix(unsigned int clampedBase, char *buf)
+{
+    if (clampedBase == 10) {
+        buf[0] = 0;
+        return buf;
+    }
+    else if (clampedBase == 2) {
+        buf[0] = '0';
+        buf[1] = 'b';
+        buf[2] = 0;
+        return &buf[2];
+    }
+    else if (clampedBase == 8) {
+        buf[0] = '0';
+        buf[1] = 'o';
+        buf[2] = 0;
+        return &buf[2];
+    }
+    else if (clampedBase == 16) {
+        buf[0] = '0';
+        buf[1] = 'x';
+        buf[2] = 0;
+        return &buf[2];
+    }
+    else {
+        buf[0] = 'b';
+        uitoa(clampedBase, buf+1, 10); // 1-2 characters written
+        const auto idx = 2 + (clampedBase > 9);
+        buf[idx] = '_';
+        buf[idx+1] = 0;
+        return &buf[idx+1];
+    }
+}
+
 } // namespace string_impl
 
 inline char * to_cstr(int num, char *buf, unsigned int base = 10u)
@@ -75,44 +110,32 @@ inline char * to_cstr(unsigned long num, char *buf, unsigned int base = 10u)
 inline char * to_cstr(unsigned long long num, char *buf, unsigned int base = 10u)
 { return ulltoa(num, buf, string_impl::clamp_base(base)); }
 
-template <typename T> char * to_cstr(T *ptr, char *buf) { return to_cstr(reinterpret_cast<std::uintptr_t>(ptr), buf, 16); }
+template <typename T> char * to_cstr(T *ptr, char *buf)
+{
+    to_cstr(reinterpret_cast<std::uintptr_t>(ptr), string_impl::base_prefix(16, buf), 16);
+    return buf;
+}
 
 template <std::integral Int> String to_string(Int num, unsigned int base)
 {
     using string_impl::append_string;
     base = std::clamp(base, 2u, 32u);
+    char prefix[8];
+    string_impl::base_prefix(base, prefix);
+    auto const reserve = [base] {
+        if (base < 8) {
+            return sizeof(Int) * 8u;  // 1 byte representable in 8 chars (255 = 0b11111111)
+        } else if (base >= 8 && base < 16) {
+            return sizeof(Int) * 3u; // 1 byte representable in 3 chars  (255 = 0o777)
+        } else if (base >= 16) {
+            return sizeof(Int) * 2u; // 1 byte representable in 2 chars (255 = 0xFF)
+        }
+    }();
 
-    if (base == 2) {
-        String s(2 + sizeof(Int) * 8u);
-        s.append("0b");
-        append_string(s, num, base);
-        return s;
-    }
-    else if (base == 8) {
-        String s(2 + sizeof(Int) * 3u);
-        s.append("0o");
-        append_string(s, num, base);
-        return s;
-    }
-    else if (base == 10) {
-        String s(sizeof(Int) * 3u);
-        append_string(s, num, base);
-        return s;
-    }
-    else if (base == 16) {
-        String s(2 + (sizeof(Int) * 2u));
-        s.append("0x");
-        append_string(s, num, base);
-        return s;
-    }
-    else {
-        String s(sizeof(Int) * 8u);
-        s.append('b');
-        append_string(s, base, 10);
-        s.append('_');
-        append_string(s, num, base);
-        return s;
-    }
+    String s(strlen(prefix) + reserve);
+    s.append(prefix);
+    append_string(s, num, base);
+    return s;
 }
 
 template <std::integral Int> String to_string(Int num) { return to_string(num, 10); }
