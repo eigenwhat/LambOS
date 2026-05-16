5c5f66a5afdd1b25c38d73ac56d58e9fdd4833b5
Martin Miralles-Cordal <axalon@rutgers.edu>
2026-05-16 00:21:32 -0400

    Debug print as well as panic in case of triple fault.

--- kernel/include/arch/i386/cpu/PageFaultPanicISR.hpp
+++ kernel/include/arch/i386/cpu/PageFaultPanicISR.hpp
@@ -12,6 +12,15 @@
 
 #include <cstdio>
 
+#define PAGE_FAULT_FORMAT "Page Fault: cr2=%x eip=%x/%@%@%@%@%@",                          \
+                          cr2,                                                             \
+                          static_cast<uint32_t>(registers.eip),                            \
+                          registers.err_code & 0x01 ? "Protection/" : "",                  \
+                          registers.err_code & 0x02 ? "Write access/" : "",                \
+                          registers.err_code & 0x04 ? "in User mode/" : "in Kernel mode/", \
+                          registers.err_code & 0x08 ? "RSVD bits set/" : "",               \
+                          registers.err_code & 0x10 ? "NX violation/" : ""
+
 class PageFaultISR : public InterruptServiceRoutine
 {
   public:
@@ -20,14 +29,8 @@ class PageFaultISR : public InterruptServiceRoutine
         DEBUG_BREAK();
         uint32_t cr2;
         asm volatile("movl %%cr2, %0" : "=r" (cr2));
-        auto message = sys::format("Page Fault: cr2=%x eip=%x/%@%@%@%@%@",
-                                   cr2,
-                                   static_cast<uint32_t>(registers.eip),
-                                   registers.err_code & 0x01 ? "Protection/" : "",
-                                   registers.err_code & 0x02 ? "Write access/" : "",
-                                   registers.err_code & 0x04 ? "in User mode/" : "in Kernel mode/",
-                                   registers.err_code & 0x08 ? "RSVD bits set/" : "",
-                                   registers.err_code & 0x10 ? "NX violation/" : "");
+        sys::debug_println(PAGE_FAULT_FORMAT);
+        auto message = sys::format(PAGE_FAULT_FORMAT);
         kernel->panic(message.cstr());
     }
 };
